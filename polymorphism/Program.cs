b13a03794e8104b7469f6fc962bc025f5b3c95a5
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-10-28 10:41:28 +0530

    first commit

---
+++ polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace polymorphism
+{
+     class Animal
+    {
+         public virtual  void animalSound()
+        {
+           Console.WriteLine("the animal sound");
+        }
+    }
+    class dog : Animal
+    {
+        public override  void animalSound()
+        {
+            Console.WriteLine("bow bow");
+        }
+    }
+     class cat : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("meow meow");
+
+        }
+    } 
+     class program
+    {
+        static void Main(string[] args)
+        {
+            Animal myanimal = new Animal();
+            Animal mydog = new dog();
+            Animal mycat = new cat();
+            myanimal.animalSound();
+            mydog.animalSound();
+            mycat.animalSound();
+        }
+    }
+ }
